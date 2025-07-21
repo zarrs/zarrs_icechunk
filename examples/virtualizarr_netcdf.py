cf7b4680108197f1f897bbff0042c9bc88d999e9
Lachlan Deakin <ljdgit@gmail.com>
2025-07-22 08:00:19 +1000

    chore: update example to `virtualizarr` 2.0 / `icechunk` 1.0 (#5)

--- examples/virtualizarr_netcdf.py
+++ examples/virtualizarr_netcdf.py
@@ -2,9 +2,9 @@
 # /// script
 # requires-python = ">=3.12"
 # dependencies = [
-#     "xarray==2025.7.0",
-#     "icechunk==0.2.18",
-#     "virtualizarr==1.3.2",
+#     "xarray==2025.6.0",
+#     "icechunk==1.0.0",
+#     "virtualizarr[icechunk,hdf]==2.0.0",
 #     "h5netcdf==1.6.3",
 #     "aiohttp==3.12.13",
 #     "requests==2.32.4",
@@ -15,15 +15,30 @@ import os
 
 import xarray as xr
 import icechunk
-from virtualizarr import open_virtual_dataset
+from virtualizarr import open_virtual_mfdataset
+from virtualizarr.parsers import HDFParser
+from virtualizarr.registry import ObjectStoreRegistry
+from obstore.store import HTTPStore
+import warnings
 
-vds = [
-    # open_virtual_dataset('examples/data/test0.nc'),
-    # open_virtual_dataset('examples/data/test1.nc'),
-    open_virtual_dataset('https://github.com/zarrs/zarrs_icechunk/raw/refs/heads/main/examples/data/test0.nc'),
-    open_virtual_dataset('https://github.com/zarrs/zarrs_icechunk/raw/refs/heads/main/examples/data/test1.nc'),
-]
-combined_vds = xr.concat(vds, dim='z', coords='minimal', compat='override')
+warnings.filterwarnings(
+    "ignore",
+    message="Numcodecs codecs are not in the Zarr version 3 specification*",
+    category=UserWarning,
+)
+
+registry = ObjectStoreRegistry({"https://github.com": HTTPStore("https://github.com")})
+
+combined_vds = open_virtual_mfdataset(
+    [
+        "https://github.com/zarrs/zarrs_icechunk/raw/refs/heads/main/examples/data/test0.nc",
+        "https://github.com/zarrs/zarrs_icechunk/raw/refs/heads/main/examples/data/test1.nc",
+    ],
+    registry=registry,
+    parser=HDFParser(),
+    concat_dim="z",
+    combine="nested",
+)
 
 storage = icechunk.local_filesystem_storage("examples/data/test.icechunk.zarr")
 repo = icechunk.Repository.create(storage=storage)
--- examples/virtualizarr_netcdf.rs
+++ examples/virtualizarr_netcdf.rs
@@ -1,6 +1,8 @@
 use std::{collections::HashMap, error::Error, path::Path, sync::Arc};
 
-use icechunk::{repository::VersionInfo, Repository, RepositoryConfig};
+use icechunk::{
+    repository::VersionInfo, virtual_chunks::VirtualChunkContainer, Repository, RepositoryConfig,
+};
 use zarrs::{
     array::{Array, ArrayMetadata},
     node::Node,
@@ -14,8 +16,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let storage =
         icechunk::new_local_filesystem_storage(&Path::new("./examples/data/test.icechunk.zarr"))
             .await?;
-    let config = RepositoryConfig::default();
-    let repo = Repository::open(Some(config), storage, HashMap::new()).await?;
+    let mut config = RepositoryConfig::default();
+    config.set_virtual_chunk_container(VirtualChunkContainer::new(
+        "https://github.com".to_string(),
+        icechunk::ObjectStoreConfig::Http(HashMap::default()),
+    )?);
+    let repo = Repository::open(
+        Some(config),
+        storage,
+        HashMap::from([("https://github.com".to_string(), None)]),
+    )
+    .await?;
 
     let session = repo
         .readonly_session(&VersionInfo::BranchTipRef("main".to_string()))
@@ -34,11 +45,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let array = Array::async_open(store.clone(), "/data").await?;
     println!("{}", array.metadata().to_string_pretty());
 
+    // Get the `add_offset` and `scale_factor` (xarray attributes)
+    // It would be preferable if virtualizarr could restore FixedScaleOffset so this doesn't need to be aware of xarray
+    let get_float_attr = |key: &str, default: f64| {
+        array.attributes().get(key).map_or(default, |value| {
+            value
+                .as_number()
+                .map_or(default, |value| value.as_f64().unwrap_or(default))
+        })
+    };
+    let add_offset = get_float_attr("add_offset", 0.0);
+    let scale_factor = get_float_attr("scale_factor", 1.0);
+
     println!(
         "{}",
         array
             .async_retrieve_array_subset_ndarray::<f64>(&array.subset_all())
             .await?
+            * scale_factor
+            + add_offset
     );
 
     Ok(())
